247754de770e58bb0847c6ec084641c6732c2880
Nicholas Ly <nichdly@gmail.com>
2023-10-26 00:14:56 -0500

    Basic deck and flashcard functionality

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,17 +18,34 @@ import {
  */
 export const mysqlTable = mysqlTableCreator((name) => `queso_${name}`);
 
-export const posts = mysqlTable(
-  "post",
+export const decks = mysqlTable(
+  "deck",
   {
     id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 100 }),
+    creatorId: varchar("creator_id", { length: 256 }),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updatedAt").onUpdateNow(),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
+  (deck) => ({
+    nameIndex: index("name_idx").on(deck.name),
+  })
+);
+
+export const cards = mysqlTable(
+  "card",
+  {
+    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+    deckId: bigint("deck_id", { mode: "number" }).references(() => decks.id),
+    front: varchar("front", { length: 256 }).notNull(),
+    back: varchar("back", { length: 256 }).notNull(),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  },
+  (card) => ({
+    deckIndex: index("deck_idx").on(card.deckId),
   })
 );

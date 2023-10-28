c7acef752a5da4a1bad862830f09ff82ae047c40
Nicholas Ly <nichdly@gmail.com>
2023-10-28 02:17:41 -0500

    Fixed duplicate line bug

--- src/app/_components/create-deck.tsx
+++ src/app/_components/create-deck.tsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 import { api } from "~/trpc/react";
 
 export default function CreateDeck() {
-export default function CreateDeck() {
   const router = useRouter();
   const [name, setName] = useState("");
 
@@ -21,24 +20,18 @@ export default function CreateDeck() {
       onSubmit={(e) => {
         e.preventDefault();
         createDeck.mutate({ name });
-        createDeck.mutate({ name });
       }}
-      className="flex gap-2 mb-4"
       className="flex gap-2 mb-4"
     >
       <input
         type="text"
         placeholder="Deck Name"
-        placeholder="Deck Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        className="outline rounded-lg px-2 py-1"
         className="outline rounded-lg px-2 py-1"
       />
       <button
         type="submit"
-        disabled={createDeck.isLoading}
-        className="outline rounded-full px-2 py-1"
         disabled={createDeck.isLoading}
         className="outline rounded-full px-2 py-1"
       >
